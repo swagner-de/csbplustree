4ec33febc4fe1f52e7b1190898df9cd1d16c737b
Sebastian Wagner <swagner-de@users.noreply.github.com>
2018-07-30 09:20:29 +0200

    added tree types

--- tests/perf_mem_fill.cpp
+++ tests/perf_mem_fill.cpp
@@ -38,17 +38,16 @@ fill_tree_rand(){
 
     ThisCsbTree_t lTreeRand;
 
-    uint64_t lCntInserted = 0;
-
     std::linear_congruential_engine<tKey , 16807UL, 0UL, 2147483647UL> lRandGenKey(_SEED_KEYS);
+    uint64_t i = 0;
     for (uint64_t c= 0; c<lLenKeyCounts; c++){
-        for (uint64_t i= lCntInserted; i< lKeyCounts[c]; i++){
+        for (/**/; i< lKeyCounts[c]; i++){
             lTreeRand.insert(lRandGenKey(),i*100);
         }
         lTreeRand.getMemoryUsage(gMemUsageStats);
 
         gCsv << lTreeRand.getFillDegree()
-             << "," << lCntInserted
+             << "," << i
              << "," << gMemUsageStats._totalBytesUsed
              << "," << gMemUsageStats._totalBytesAlloc
              << "," << sizeof(tKey)
@@ -57,19 +56,7 @@ fill_tree_rand(){
              << ",random"
                 << endl;
 
-        lCntInserted = lKeyCounts[c];
     }
-
-    lTreeRand.getMemoryUsage(gMemUsageStats);
-    gCsv << lTreeRand.getFillDegree()
-         << "," << lCntInserted
-         << "," << gMemUsageStats._totalBytesUsed
-         << "," << gMemUsageStats._totalBytesAlloc
-         << "," << sizeof(tKey)
-         << "," << sizeof(tTid)
-         << "," << kSzeNode
-         << ",random"
-         << endl;
 }
 
 template <class tKey, class tTid, uint16_t kSzeNode>
@@ -81,8 +68,9 @@ fill_tree_incr(){
 
     uint64_t lCntInserted = 0;
 
+    uint64_t i = 0;
     for (uint64_t c= 0; c<lLenKeyCounts; c++){
-        for (uint64_t i= lCntInserted; i< lKeyCounts[c]; i++){
+        for (/**/; i< lKeyCounts[c]; i++){
             lTreeRand.insert(i,i*100);
         }
 
@@ -96,20 +84,7 @@ fill_tree_incr(){
              << "," << kSzeNode
              << ",incr"
              << endl;
-
-        lCntInserted = lKeyCounts[c];
     }
-
-    lTreeRand.getMemoryUsage(gMemUsageStats);
-    gCsv << lTreeRand.getFillDegree()
-         << "," << lCntInserted
-         << "," << gMemUsageStats._totalBytesUsed
-         << "," << gMemUsageStats._totalBytesAlloc
-         << "," << sizeof(tKey)
-         << "," << sizeof(tTid)
-         << "," << kSzeNode
-         << ",incr"
-         << endl;
 }
 
 template <class tKey, class tTid, uint16_t kSzeNode>
@@ -120,9 +95,9 @@ fill_tree_decr(){
     ThisCsbTree_t lTreeRand;
 
     uint64_t lCntInserted = 0;
-
+    uint64_t i = 0;
     for (uint64_t c= 0; c<lLenKeyCounts; c++){
-        for (uint64_t i= lCntInserted; i< lKeyCounts[c]; i++){
+        for (/**/; i< lKeyCounts[c]; i++){
             lTreeRand.insert(UINT64_MAX-i,i*100);
         }
         lTreeRand.getMemoryUsage(gMemUsageStats);
@@ -138,17 +113,6 @@ fill_tree_decr(){
              << endl;
         lCntInserted = lKeyCounts[c];
     }
-
-    lTreeRand.getMemoryUsage(gMemUsageStats);
-    gCsv << lTreeRand.getFillDegree()
-         << "," << lCntInserted
-         << "," << gMemUsageStats._totalBytesUsed
-         << "," << gMemUsageStats._totalBytesAlloc
-         << "," << sizeof(tKey)
-         << "," << sizeof(tTid)
-         << "," << kSzeNode
-         << ",decr"
-         << endl;
 }
 
 
@@ -169,14 +133,57 @@ int main(int argc, char *argv[]){
 
     gCsv << "fillDegree,countKeys,MemoryUsed,MemoryAllocated,KeySize,TidSize,CacheLines,method" << endl;
 
-    for (uint16_t i = 0; i<7; i++){
-        fill_tree_rand<uint64_t, uint32_t, 3>();
-        fill_tree_rand<uint64_t, uint64_t, 3>();
-        fill_tree_incr<uint64_t, uint32_t, 3>();
-        fill_tree_incr<uint64_t, uint64_t, 3>();
-        fill_tree_decr<uint64_t, uint32_t, 3>();
-        fill_tree_decr<uint64_t, uint64_t, 3>();
-    }
+
+    fill_tree_rand<uint64_t, uint32_t, 1>();
+    fill_tree_rand<uint64_t, uint64_t, 1>();
+    fill_tree_rand<uint64_t, uint32_t, 2>();
+    fill_tree_rand<uint64_t, uint64_t, 2>();
+    fill_tree_rand<uint64_t, uint32_t, 3>();
+    fill_tree_rand<uint64_t, uint64_t, 3>();
+    fill_tree_rand<uint64_t, uint32_t, 4>();
+    fill_tree_rand<uint64_t, uint64_t, 4>();
+    fill_tree_rand<uint64_t, uint32_t, 5>();
+    fill_tree_rand<uint64_t, uint64_t, 5>();
+    fill_tree_rand<uint64_t, uint32_t, 6>();
+    fill_tree_rand<uint64_t, uint64_t, 6>();
+    fill_tree_rand<uint64_t, uint32_t, 7>();
+    fill_tree_rand<uint64_t, uint64_t, 7>();
+    fill_tree_rand<uint64_t, uint32_t, 8>();
+    fill_tree_rand<uint64_t, uint64_t, 8>();
+
+    fill_tree_incr<uint64_t, uint32_t, 1>();
+    fill_tree_incr<uint64_t, uint64_t, 1>();
+    fill_tree_incr<uint64_t, uint32_t, 2>();
+    fill_tree_incr<uint64_t, uint64_t, 2>();
+    fill_tree_incr<uint64_t, uint32_t, 3>();
+    fill_tree_incr<uint64_t, uint64_t, 3>();
+    fill_tree_incr<uint64_t, uint32_t, 4>();
+    fill_tree_incr<uint64_t, uint64_t, 4>();
+    fill_tree_incr<uint64_t, uint32_t, 5>();
+    fill_tree_incr<uint64_t, uint64_t, 5>();
+    fill_tree_incr<uint64_t, uint32_t, 6>();
+    fill_tree_incr<uint64_t, uint64_t, 6>();
+    fill_tree_incr<uint64_t, uint32_t, 7>();
+    fill_tree_incr<uint64_t, uint64_t, 7>();
+    fill_tree_incr<uint64_t, uint32_t, 8>();
+    fill_tree_incr<uint64_t, uint64_t, 8>();
+
+    fill_tree_decr<uint64_t, uint32_t, 1>();
+    fill_tree_decr<uint64_t, uint64_t, 1>();
+    fill_tree_decr<uint64_t, uint32_t, 2>();
+    fill_tree_decr<uint64_t, uint64_t, 2>();
+    fill_tree_decr<uint64_t, uint32_t, 3>();
+    fill_tree_decr<uint64_t, uint64_t, 3>();
+    fill_tree_decr<uint64_t, uint32_t, 4>();
+    fill_tree_decr<uint64_t, uint64_t, 4>();
+    fill_tree_decr<uint64_t, uint32_t, 5>();
+    fill_tree_decr<uint64_t, uint64_t, 5>();
+    fill_tree_decr<uint64_t, uint32_t, 6>();
+    fill_tree_decr<uint64_t, uint64_t, 6>();
+    fill_tree_decr<uint64_t, uint32_t, 7>();
+    fill_tree_decr<uint64_t, uint64_t, 7>();
+    fill_tree_decr<uint64_t, uint32_t, 8>();
+    fill_tree_decr<uint64_t, uint64_t, 8>();
 
 
 
